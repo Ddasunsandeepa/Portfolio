fbcee70a0e6d76e7fcc1ce758b32746d95171b64
Dasun Sandepa <dsk.sandeep.987@gmail.com>
2025-12-27 13:28:01 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-black">Home</main>
+      <main className="bg-black">
+        Home
+        <h1 className="text-3xl font-bold text-blue-600">Hello</h1>
+      </main>
     </>
   );
 }
